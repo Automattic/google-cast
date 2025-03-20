2eb5b899beaf20f95ead39a72a24257acbfb4fae
Daniele Bogo <me@bogodaniele.com>
2025-03-20 16:36:32 +0000

    Add protobuf

--- Package.swift
+++ Package.swift
@@ -14,12 +14,21 @@ let package = Package(
             targets: ["GoogleCast"]
         )
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.0.0")
+    ],
     targets: [
         .binaryTarget(
             name: "GoogleCast",
             url: "https://dl.google.com/dl/chromecast/sdk/ios/GoogleCastSDK-ios-4.8.3_static.zip",
             checksum: "b53cc17671154f5ff4ba99165a8b9a6d677e8165715830b91cc1857634b33901"
+        ),
+        .target(
+            name: "GoogleCastWrapper",
+            dependencies: [
+                .target(name: "GoogleCast"),
+                .product(name: "SwiftProtobuf", package: "swift-protobuf")
+            ]
         )
     ]
 )
